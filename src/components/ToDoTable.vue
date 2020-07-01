e85234626f91fd1aff35cb02b821f077e2651716
QSFTW <gao.qiangsheng.2015@vjc.sg>
2020-07-01 13:17:53 +0800

    'Add' functionality added

--- src/components/ToDoTable.vue
+++ src/components/ToDoTable.vue
@@ -1,25 +1,53 @@
 <template>
-  <el-table :data="tableData" style="width: 100%;" max-height="100%" margin="auto">
-    <el-table-column type="index" width="50"></el-table-column>
-    <el-table-column prop="Subject" label="Subject" width="120"></el-table-column>
-    <el-table-column prop="Description" label="Description" width="360"></el-table-column>
-    <el-table-column label="Operations" width="120">
-      <template slot-scope="scope">
-        <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData)"
-          type="text"
-          size="small"
-        >Remove</el-button>
-        <el-button type="text" size="small" @click="editRow(scope.$index, tableData)">Edit</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <div style="margin: 10px 0px" align="left">
+      <el-button @click.native.prevent="modalOpen = true">Add</el-button>
+      <el-dialog title="Add new Todo Item" :visible.sync="modalOpen">
+        <el-form :model="todoForm">
+          <el-form-item label="Subject" :label-width="'120px'">
+            <el-input v-model="todoForm.Subject" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="Description" :label-width="'120px'">
+            <el-input v-model="todoForm.Description" placeholder="Describe your Todo Item"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="addRowCancel(todoForm)">Cancel</el-button>
+          <el-button type="primary" @click="addRow(todoForm)">Confirm</el-button>
+        </span>
+      </el-dialog>
+    </div>
+    <el-table :data="tableData" style="width: 100%;" max-height="100%" margin="auto">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="Subject" label="Subject" width="120"></el-table-column>
+      <el-table-column prop="Description" label="Description" width="360"></el-table-column>
+      <el-table-column label="Operations" width="120">
+        <template slot-scope="scope">
+          <el-button
+            @click.native.prevent="deleteRow(scope.$index, tableData)"
+            type="text"
+            size="small"
+          >Remove</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click.native.prevent="editRow(scope.$index, tableData)"
+          >Edit</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
 export default {
   name: "ToDoTable",
   data() {
     return {
+      modalOpen: false,
+      todoForm: {
+        Subject: "",
+        Description: ""
+      },
       tableData: [
         {
           Subject: "Tom",
@@ -58,7 +86,7 @@ export default {
     },
 
     editRow(index, tableData) {
-      this.$prompt("Please describe the todo item", tableData[index].Summary, {
+      this.$prompt("Please describe the todo item", tableData[index].Subject, {
         confirmButtonText: "OK",
         cancelButtonText: "Cancel"
       })
@@ -75,6 +103,20 @@ export default {
             message: "Edit canceled"
           });
         });
+    },
+    addRowCancel(todoForm) {
+      todoForm.Subject = "";
+      todoForm.Description = "";
+      this.modalOpen = false;
+    },
+    addRow(todoForm) {
+      this.tableData.unshift({
+        Subject: todoForm.Subject,
+        Description: todoForm.Description
+      });
+      todoForm.Subject = "";
+      todoForm.Description = "";
+      this.modalOpen = false;
     }
   }
 };
